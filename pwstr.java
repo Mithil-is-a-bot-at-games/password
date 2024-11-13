19086084115753acfa779a9492812f3770000202
ItsMissle <72278598+Mithil-is-a-bot-at-games@users.noreply.github.com>
2024-11-13 13:30:14 -0500

    Add files via upload

--- pwstr.java
+++ pwstr.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 class Main{
     public static void score(boolean upper,boolean length,boolean lower,boolean dig,boolean symbol){
+        //initializing score variable
         int score = 0;
+        //adding a point for every requirement the password meets
         if(upper == true){
             score +=1;
-        }else{System.out.println("Your Password Should Contain Atleast One UpperCase Letter.");}
+        }else{System.out.println("Your Password Should Contain Atleast One UpperCase Letter.");}//printing out what the user needs to change in their password
         if(lower == true){
             score+=1;
         }else{System.out.println("Your Password Should Contain Atleast One LowerCase Letter.");}
@@ -17,16 +19,20 @@ class Main{
         if(symbol == true){
             score +=1;
         }else{System.out.println("Your Password Should Contain Atleast One Special Character.");}
+        //printing out final password score
         System.out.println("Your Password strength is a " + score + " out of 5");
 
     }
     public static void validity(String password){
+        //intializing variables
         boolean upper = false;
         boolean length = false;
         boolean lower = false;
         boolean dig = false;
         boolean symbol = false;
+        //swapping input from string to character
         for(char c :password.toCharArray()){
+            //checking if input meets password requirements
             if(Character.isUpperCase(c)){
                 upper = true;
             }else if(Character.isLowerCase(c)){
@@ -35,29 +41,32 @@ class Main{
                 dig = true;
             }
         }
+        //looping through user input
         for (int i = 0; i < password.length(); i++) {
  
-            // Checking the character for not being a
-            // letter,digit or space
+            //checking input for special characters
             if (!Character.isDigit(password.charAt(i))
                 && !Character.isLetter(password.charAt(i))
                 && !Character.isWhitespace(password.charAt(i))) {
-                // Incrementing the countr for spl
-                // characters by unity
                 symbol = true;
             }
         }
+        //checking if password length meets the requirements
         if(password.length() >= 12){
             length = true;
         }
+        //calling scoring function
         score(upper,length,lower,dig,symbol);
 
     }
     public static void main(String[] args) {
+        //intializing variables
         Scanner input = new Scanner(System.in);
         String pw;
+        //getting user input 
         System.out.println("Enter a password:");
         pw = input.nextLine();
+        //calling function to check is input meets requirements
         validity(pw);
 
 
